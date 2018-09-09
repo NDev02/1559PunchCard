ceaef1ac835ad3d9478918b8cab17ac7161be639
Nathan Gordon <ngordondeveloper@gmail.com>
2018-09-09 10:50:59 -0400

    Update punchcard.js

--- punchcard.js
+++ punchcard.js
@@ -31,7 +31,7 @@ function signIn()
 
 function signOut()
 {
-    var date = new Date(Date());
+    var date = new Date();
     var dateStr = `${date.getMonth()}-${date.getDay()}-${date.getFullYear()}`;
 	alert(dateStr);
     var timeStr = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
